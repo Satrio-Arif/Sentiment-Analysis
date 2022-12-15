09ab14bcad87fd5d7650bfa04d96e0e9cb6ae4e5
satrioarifb <satrioarif@student.ub.ac.id>
2022-12-15 18:57:11 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import model_job as model
+import predict_text as predict
+import streamlit as st
+import time
+import webbrowser
+
+st.title('Analisis Sentimen Ulasan Pelanggan Kober Mie Setan Malang')
+
+st.header('Prediksi Sentimen dan Aspek Ulasan')
+text = st.text_input('Masukkan Teks Ulasan', 'mie enak')
+if st.button('Mulai Analisis'):
+    if text.strip()=='' :
+        st.error('Cek kembali teks ulasan', icon="🚨")
+    else:
+        with st.spinner('Wait for it...'):
+            time.sleep(0.1)
+            st.success('Success!')
+        
+        teks_df = predict.job(text)
+        st.table(teks_df)
+else:
+    st.write('')
+
+
+# st.header('Prediksi Sentimen dan Aspek Ulasan Google Review Terbaru')
+
+# # url = 'https://datastudio.google.com/reporting/defe47e1-3401-4820-97a2-93ac0c6d19b0'
+# link=' ### Buka Halaman Dashboard Analisis : [Dashboard](https://datastudio.google.com/reporting/defe47e1-3401-4820-97a2-93ac0c6d19b0)'
+# st.markdown(link,unsafe_allow_html=True)
+# # if st.button('Buka Halaman Dashboard Analisis'):
+# #     webbrowser.open_new_tab(url)
+# st.caption('Update data dashboard membutuhkan waktu 15-60 menit atau klik refresh data pada tombol option (⋮)')
+
+# if st.button('Mulai Analisis Data Ulasan Terbaru'):    
+#     with st.spinner('Sedang mengambil teks ulasan ...'):
+#         data=model.job()
+#         st.success('Success!')
+#     st.subheader('Hasil Analisis Data Ulasan Terbaru')
+#     st.table(data)
+# else:
+#     st.write('')
+
---
+++ model_job.py
@@ -0,0 +1,76 @@
+import os.path
+import requests
+import json
+import pandas as pd
+from sklearn import model_selection
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+# import writeSheet as write
+
+def job() :
+  df = pd.DataFrame()
+
+  def scrapData() :
+    url = 'https://api.apify.com/v2/actor-tasks/lavender_laptop~crawler-google-places-task/run-sync-get-dataset-items?token=apify_api_bFpVGG1khyLOt2CisgFIZvh4YSthSM3Ddz4B&method=POST'
+    response = requests.post(url)
+    data = json.loads(response.text)
+
+    text=[]
+    username=[]
+    date=[]
+
+    for review in data:
+      if review['text'] is not None:
+        if len(review['text']) >= 2:
+          text.append(review['text'])
+          username.append(review['name'])
+          date.append(review['publishedAtDate'][0:10])
+      
+    
+
+    df['ULASAN'] = text
+    df['NAMA'] = username
+    df['TANGGAL'] = date
+
+  scrapData()
+    
+  loaded_model = pickle.load(open(os.path.basename('finalized_model.sav'), 'rb'))
+
+
+  # Create feature vectors
+  vectorizer = TfidfVectorizer(min_df = 5,
+                              max_df = 0.8,
+                              sublinear_tf = True,
+                              use_idf = True)
+
+  vectorizer = pickle.load(open(  os.path.basename('vectorizer.pickle'), 'rb'))
+
+
+  def model_pred(string) :
+    test = string.split("-")
+    test_vector = vectorizer.transform(test)
+    if loaded_model.predict(test_vector)[0] == 1 :
+      return "Positif"
+    else :
+      return "Negatif"
+      
+  # load the model from disk
+  aspek_model = pickle.load(open(  os.path.basename('aspek_model.sav'), 'rb'))
+
+  review_test = ['makanan enak']
+  test_vector = vectorizer.transform(review_test)
+  aspek_model.predict(test_vector)
+
+  def aspek_pred(string) :
+    test = string.split("-")
+    test_vector = vectorizer.transform(test)
+    return aspek_model.predict(test_vector)[0]
+
+  df['Aspek'] = df['ULASAN'].apply(aspek_pred)
+  # write.insert(df)
+  return df
+
+# df = job()
+
+# print(df)
+
